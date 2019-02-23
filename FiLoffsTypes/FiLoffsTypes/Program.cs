f720101f88bf196ba6f7a1db070e5f9fc8d2b6eb
FiLoffs <filod72@gmail.com>
2019-02-23 12:23:23 +0200

    Lesson 2. project Types

---
+++ FiLoffsTypes/FiLoffsTypes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiLoffsTypes
+{
+    class Class1
+    {
+        static void Main(string[] args)
+        {
+            testing tm = new Testing();
+            Console.WriteLine("Testing.Who Test");
+            tm.WhoTest();
+            Console.WriteLine("Testing.Back Test");
+            tm.BackTest();
+            Console.WriteLine("Testing.OLoad Test");
+            tm.OLoadTest();
+            Console.WriteLine("Testing.ToString Test");
+            tm.ToStringTest();
+            Console.WriteLine("Testing.FromString Test");
+            tm.FromStringTest();
+            Console.WriteLine("Testing.CheckUncheck Test");
+            tm.CheckUncheckTest();
+
+        }
+    }
+}
