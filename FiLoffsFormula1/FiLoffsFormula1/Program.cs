850a65e6a164c8842ab06c8ae3333fb12a52c565
FiLoffs <filod72@gmail.com>
2019-02-25 10:00:32 +0200

    Добавил присвоение переменных генератором сл. чисел

--- FiLoffsFormula1/FiLoffsFormula1/Program.cs
+++ FiLoffsFormula1/FiLoffsFormula1/Program.cs
@@ -10,12 +10,31 @@ namespace FiLoffsFormula1
     {
         static void Main(string[] args)
         {
-            double a=5, b=2, c=3, d;
-            //добавить ввод с клавиатуры
-            d = (b + Math.Sqrt(b * b + 4 * a * c)) / 2 * a - Math.Pow(a, 3) * c + Math.Pow(b, -2);
-            Console.WriteLine("d = "+d);
+            Random x = new Random();           
 
-
+            task1();
+                                   
         }
+
+        public double RandomVar()
+        {
+            Random x = new Random();
+            double a = Convert.ToDouble(x.Next(100));
+            double b = Convert.ToDouble(x.Next(100));
+            double c = Convert.ToDouble(x.Next(100));
+            double d = Convert.ToDouble(x.Next(100));
+            return;
+        }
+
+        public static void task1()
+        {
+            double a =0, b=0, c=0, d=0;
+            RandomVar(a,b,c);
+            d = (b + Math.Sqrt(b * b + 4 * a * c)) / 2 * a - Math.Pow(a, 3) * c + Math.Pow(b, -2);
+            Console.WriteLine("a = " + a + ", b = " + b + ", c = " + c);
+            Console.WriteLine("d = " + d);
+        }
+
+
     }
 }
