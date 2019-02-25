831d8a04383531d58b9a8cb6c0ba40864098071c
FiLoffs <filod72@gmail.com>
2019-02-25 09:14:42 +0200

    Занятие 2 Домашнее задание. Формулы

---
+++ FiLoffsFormula1/FiLoffsFormula1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FiLoffsFormula1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a=5, b=2, c=3, d;
+            //добавить ввод с клавиатуры
+            d = (b + Math.Sqrt(b * b + 4 * a * c)) / 2 * a - Math.Pow(a, 3) * c + Math.Pow(b, -2);
+            Console.WriteLine("d = "+d);
+
+
+        }
+    }
+}
