0d234e8f419e625114412b1e68e04005e16fd876
FiLoffs <filod72@gmail.com>
2019-02-27 12:54:42 +0200

    Добавлено задание 5, 7,13,16

--- FiLoffsFormula1/FiLoffsFormula1/Program.cs
+++ FiLoffsFormula1/FiLoffsFormula1/Program.cs
@@ -13,6 +13,10 @@ namespace FiLoffsFormula1
             task1();
             task2();
             task3();
+            task5();
+            task7();
+            task13();
+            task16();
 
                                    
         }
@@ -72,9 +76,77 @@ namespace FiLoffsFormula1
             x = Convert.ToDouble(r.Next(360));
             y = Convert.ToDouble(r.Next(360));
 
-            result = Math.Tan((Math.Sin(x)+Math.Cos(y))/(Math.Sin(x) - Math.Cos(y)))*x*y;
+            result = Math.Tan((Math.Sin(x)+Math.Cos(y))/((Math.Sin(x) - Math.Cos(y)))*x*y);
             Console.WriteLine("task 3");
             Console.WriteLine("x = " + x + ", y = " + y);
+            Console.WriteLine("result = " + result);
+            Console.WriteLine();
+            Console.WriteLine("================");
+            Console.WriteLine();
+        }
+
+        public static void task5()
+        {
+            double x, y, e, result;
+            //RandomVar(a,b,c);
+            Random r = new Random();
+            x = Convert.ToDouble(r.Next(100));
+            y = Convert.ToDouble(r.Next(100));
+            e = Convert.ToDouble(r.Next(100));
+
+            result = (3 + Math.Pow(e, y-1))/(1+ x*x*Math.Abs(y - Math.Tan(x)));
+            Console.WriteLine("task 5");
+            Console.WriteLine("x = " + x + ", y = " + y + ", e = " + e);
+            Console.WriteLine("result = " + result);
+            Console.WriteLine();
+            Console.WriteLine("================");
+            Console.WriteLine();
+        }
+
+        public static void task7()
+        {
+            double x, y, result;
+            //RandomVar(a,b,c);
+            Random r = new Random();
+            x = Convert.ToDouble(r.Next(100));
+            y = Convert.ToDouble(r.Next(100));
+            
+            result = Math.Log(Math.Abs((y-Math.Sqrt(Math.Abs(x)))*(x-y/(x+x*x/4))));
+            Console.WriteLine("task 7");
+            Console.WriteLine("x = " + x + ", y = " + y);
+            Console.WriteLine("result = " + result);
+            Console.WriteLine();
+            Console.WriteLine("================");
+            Console.WriteLine();
+        }
+
+        public static void task13()
+        {
+            double x, y, result;
+            //RandomVar(a,b,c);
+            Random r = new Random();
+            x = Convert.ToDouble(r.Next(100));
+            y = Convert.ToDouble(r.Next(100));
+
+            result = Math.Cos(x) / (Math.PI - 2 * x) + 16 * x * Math.Cos(x * y) - 2;
+            Console.WriteLine("task 13");
+            Console.WriteLine("x = " + x + ", y = " + y);
+            Console.WriteLine("result = " + result);
+            Console.WriteLine();
+            Console.WriteLine("================");
+            Console.WriteLine();
+        }
+
+        public static void task16()
+        {
+            double x, result;
+            //RandomVar(a,b,c);
+            Random r = new Random();
+            x = Convert.ToDouble(r.Next(100));
+
+            result = Math.Abs(Math.Pow(x, 2) - Math.Pow(x, 3)) - 7 * x / (Math.Pow(x, 3) - 15 * x);
+            Console.WriteLine("task 16");
+            Console.WriteLine("x = " + x);
             Console.WriteLine("result = " + result);
             Console.WriteLine();
             Console.WriteLine("================");
